3e1bd567a274146830d599f6e312643159a722c1
ArthurM-01 <vinips4.195@gmail.com>
2025-07-13 18:27:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text)
 }
 
-// AIzaSyCBjNFjImQNZAOj7qw6fESeyAEwEFBeuF0 Chave de API
+
 const perguntarAI = async (question, game, apiKey) => {
   const model = "gemini-2.5-flash"
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
